c9b740c536c8bf2c5e3532630f48f5ad5e8d02f4
Henry Philpotts <hphilpotts@gmail.com>
2023-02-17 17:25:30 +0000

    FoodItem model added.

---
+++ app/models/Food-item.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const foodItemCategories = ['green vegetables', 'salad vegetables', 'salad leaves', 'root vegetables', 'onions & friends', 'berries', 'orchard fruits', 'citrus fruits', 'exotic fruits', 'grains', 'nuts & seeds', 'miscellaneous'];
+
+const foodItemSchema = mongoose.Schema({
+    userAddedBy: { type: mongoose.Types.ObjectId, ref: 'User'},
+    foodItemName: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    foodItemCategory: {
+        type: String,
+        enum: foodItemCategories,
+        required: true
+    }
+
+})
+
+const FoodItem = mongoose.model("FoodItem", foodItemSchema);
+
+module.exports = FoodItem;

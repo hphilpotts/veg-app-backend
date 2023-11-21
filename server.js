5f0198240a10602f1fa61eb3a8912b46ce1b30c9
Henry Philpotts <hphilpotts@gmail.com>
2023-11-21 20:06:58 +0000

    server.js updated.

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
-// * server.js - import the app and assign HTTP server, connect to db, entry point of app
-
 const { app } = require("./app");
 
 const PORT = process.env.PORT;
+
 app.listen(PORT, () => {
-    try {
-        console.log(`Hello-Express Application is running on port ${PORT}`);
-    } catch (error) {
-        console.error(error);
-    }
+
+        if (PORT == undefined) {
+                throw new Error('Environment variable `PORT` is undefined.');
+        }
+
+        try {
+                console.log(`Album Review App is running on:\x1b[36m ${PORT} \x1b[0m`);
+        } catch (error) {
+                console.error(error);
+        }
+        
 });

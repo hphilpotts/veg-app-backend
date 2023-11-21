f1baa374572c13f2fc9b3768fe93528eb3227e17
Henry Philpotts <hphilpotts@gmail.com>
2023-11-21 20:17:58 +0000

    Update to mongodb util.

--- app/utils/connect-local-mongodb.js
+++ app/utils/connect-local-mongodb.js
@@ -1,43 +1,35 @@
-// Local MongoDB connection separated out into its own file in order to absract details 
 const mongoose = require('mongoose');
 require('dotenv').config();
 
 const makeLocalDBConnection = () => {
-    mongoose.set('strictQuery', true); // this and second param in .connect() below prevent deprecation warnings
-    const mongoDBLocalURL = process.env.MONGODB_LOCAL_URL;
-    mongoose.connect(
-        mongoDBLocalURL,
-        {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        },
-        () => {
-            if (mongoose.connection.name === undefined) {
-                console.error('Mongoose connection with MongoDB not established!');
-            } else {
-                console.log('Connected to local MongoDB! Database:', mongoose.connection.name, 'on PORT', mongoose.connection.port);
-            }
-        }
-    )
-}
+        mongoose.set('strictQuery', true); // this and second param in .connect() below prevent deprecation warnings
+        const mongoDBLocalURL = process.env.MONGODB_LOCAL_URL;
+        mongoose.connect(mongoDBLocalURL, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+                if (mongoose.connection.name === undefined) {
+                        console.error('Mongoose connection with MongoDB not established!');
+                } else {
+                        console.log('Connected to local MongoDB! Database:', mongoose.connection.name, 'on PORT', mongoose.connection.port);
+                }
+        });
+};
 
 const checkDBConnectionStatus = () => {
-    mongoose.connection.on('connected', function () {
-        console.log('Mongoose default connection open!');
-    })
-    mongoose.connection.on('error', function (err) {
-        console.error('Mongoose default connection error:', err);
-    })
-    mongoose.connection.on('disconnected', function () {
-        console.warn('\nMongoose default connection disconnected!');
-    })
+        mongoose.connection.on('connected', function () {
+                console.log('Mongoose default connection open!');
+        });
+        mongoose.connection.on('error', function (err) {
+                console.error('Mongoose default connection error:', err);
+        });
+        mongoose.connection.on('disconnected', function () {
+                console.warn('\nMongoose default connection disconnected!');
+        });
 }
 
 process.on('SIGINT', function () {
-    mongoose.connection.close(function () {
-        console.log('Mongoose default connection disconnected through app termination');
-        process.exit(0);
-    });
+        mongoose.connection.close(function () {
+                console.log('Mongoose default connection disconnected through app termination');
+                process.exit(0);
+        });
 });
 
 module.exports = { makeLocalDBConnection, checkDBConnectionStatus };
--- server.js
+++ server.js
@@ -6,12 +6,12 @@ app.listen(PORT, () => {
 
         if (PORT == undefined) {
                 throw new Error('Environment variable `PORT` is undefined.');
-        }
+        };
 
         try {
                 console.log(`Album Review App is running on:\x1b[36m ${PORT} \x1b[0m`);
         } catch (error) {
                 console.error(error);
-        }
-        
+        };
+
 });

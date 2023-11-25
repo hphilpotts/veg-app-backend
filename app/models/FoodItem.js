be02807923941f2b8c7dc09802c25dc336b55888
Henry Philpotts <hphilpotts@gmail.com>
2023-11-25 19:35:53 +0000

    Updated FoodItem model.

--- app/models/FoodItem.js
+++ app/models/FoodItem.js
@@ -1,28 +1,46 @@
 const mongoose = require('mongoose');
 
-const foodItemCategories = ['green vegetables', 'salad vegetables', 'salad leaves', 'root vegetables', 'onions & friends', 'berries', 'orchard fruits', 'citrus fruits', 'exotic fruits', 'grains', 'nuts & seeds', 'miscellaneous'];
+const foodItemCategories = [
+    "Green Vegetables",
+    "Salad Vegetables",
+    "Salad Leaves",
+    "Root Vegetables",
+    "Onions & Friends",
+    "Legumes & Pulses",
+    "Nuts & Seeds",
+    "Grains & Cereals",
+    "Orchard Fruits",
+    "Citrus Fruits",
+    "Exotic Fruits",
+    "Berries",
+    "Other Fruits",
+    "Herbs",
+    "Spices",
+    "Sweeteners",
+    "Oils",
+    "Miscellaneous"
+];
 
 const foodItemSchema = mongoose.Schema({
-    addedBy: { type: mongoose.Types.ObjectId, ref: 'User' },
     name: {
         type: String,
         required: true,
         unique: true
-    },
-    icon: {
-        type: String,
-        required: true
     },
     category: {
         type: String,
         enum: foodItemCategories,
         required: true
     },
-    favouritedBy: [{
-        type: mongoose.Types.ObjectId, ref: 'User'
-    }]
-
-})
+    addedBy: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User'
+    },
+    favourited: {
+        type: Boolean,
+        default: false
+    }
+});z
 
 const FoodItem = mongoose.model("FoodItem", foodItemSchema);
 

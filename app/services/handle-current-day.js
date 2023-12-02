01b6ee6a24c2ca82d7931518bad5971f32cc6630
Henry Philpotts <hphilpotts@gmail.com>
2023-12-02 14:37:33 +0000

    Updates to handle-current-day services.

--- app/services/handle-current-day.js
+++ app/services/handle-current-day.js
@@ -1,13 +1,22 @@
-
-exports.getFirstDayOfCurrentWeek = () => {
-    const today = new Date();
-    const firstDay = new Date(today.setDate(today.getDate() - today.getDay()));
-    const firstDayAtZeroHundred = new Date(firstDay.setHours(0, 0, 0, 0));
-    return firstDayAtZeroHundred;
+const dayNumberToName = {
+    1: "monday",
+    2: "tuesday",
+    3: "wednesday",
+    4: "thursday",
+    5: "friday",
+    6: "saturday",
+    0: "sunday"
 }
 
-exports.getCurrentDayOfTheWeek = () => {
+exports.getWeekCommencing = inputDate => { // can take a simple YYYY-MM-DD string
+    const today = new Date(inputDate);
+    // ternary accounts for JS default where Sunday is the first day of the week, instead gets the Monday week commencing
+    const mondayCurrentWeek = new Date(today.setDate(today.getDate() - (today.getDay() === 0 ? 6 : today.getDay() - 1) ));
+    return weekCommencingDate = new Date(mondayCurrentWeek.setHours(0, 0, 0, 0));
+}
+
+exports.getCurrentDay = () => {
     const currentDate = new Date();
     const todayAsNumber = currentDate.getDay();
-    return todayAsNumber.toString();
+    return dayNumberToName[todayAsNumber];
 }

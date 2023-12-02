1c7d221fe33e2c1e7a004ec07776b8e3a4e163c4
Henry Philpotts <hphilpotts@gmail.com>
2023-12-02 18:15:34 +0000

    Added week_byDate_get.

--- app/controllers/week.js
+++ app/controllers/week.js
@@ -2,11 +2,8 @@ const Week = require('../models/Week');
 const dateServices = require('../services/dates');
 
 exports.week_create_post = async (req, res) => {
-    const { user, inputDate } = { ...req.body};
-    console.log("inputDate" + inputDate)
-    const weekCommencing = dateServices.getWeekCommencing(inputDate);
-
-    console.log(weekCommencing)
+    const { user, date } = { ...req.body};
+    const weekCommencing = dateServices.getWeekCommencing(date);
 
     try {
 
@@ -49,135 +46,124 @@ exports.week_index_get = (req, res) => {
         });
 };
 
-// exports.week_indexByUser_put = async (req, res) => {
+exports.week_byDate_get = (req, res) => {
+    const { user, date } = { ...req.query };
+    const weekCommencing = dateServices.getWeekCommencing(date);
+    Week.findOne({ user, weekCommencing })
+        .then(Week => {
+            res.status(200).json({ Week });
+        })
+        .catch(error => {
+            console.error(error);
+            res.status(400).json({ "message": "Error getting week by date, please try again later" })
+        });
+};
+
+// exports.week_currentDay_put = (req, res) => {
 //     const { userOwner } = req.body;
-//     Week.find({ userOwner }).sort('-weekCommencing')
-//         .then(Weeks => {
-//             res.json({ Weeks }).status(200);
+//     const currentDay = handleCurrent.getCurrentDayOfTheWeek();
+
+//     Week.findOne({ userOwner, weekCommencing })
+//         .then(Week => {
+//             const entriesFromToday = Week[`${currentDay}`];
+//             res.json({ entriesFromToday }).status(200);
 //         })
 //         .catch(err => {
 //             console.error(err);
-//             res.json({ "message": "Error getting all weeks, please try again later." }).status(400);
+//             res.json({ "message": "Error getting today's entries, please try again later." }).status(400);
 //         });
 // }
 
-exports.week_currentWeek_put = async (req, res) => {
-    const { userOwner } = req.body;
-    await Week.findOne({ userOwner, weekCommencing })
-        .then(Week => {
-            res.json({ Week }).status(200);
-        })
-        .catch(err => {
-            console.error(err);
-            res.json({ "message": "Error getting current week, please try again later." }).status(400);
-        });
-}
+// exports.week_detailById_put = (req, res) => {
+//     const { id, userOwner } = req.body;
+//     Week.findOne({ id, userOwner })
+//         .then(Week => {
+//             res.json({ Week }).status(200);
+//         })
+//         .catch(err => {
+//             console.error(err);
+//             res.json({ "message": "Error getting week by id, please try again later." }).status(400);
+//         });
+// }
 
-exports.week_currentDay_put = (req, res) => {
-    const { userOwner } = req.body;
-    const currentDay = handleCurrent.getCurrentDayOfTheWeek();
+// exports.week_dailyDetailById_put = (req, res) => {
+//     const { id, day, userOwner } = req.body;
+//     Week.findOne({ id, userOwner })
+//         .then(Week => {
+//             const dayDetail = Week[day];
+//             res.json({ dayDetail }).status(200);
+//         })
+//         .catch(err => {
+//             console.error(err);
+//             res.json({ "message": "Error getting day by id, please try again later." }).status(400);
+//         });
+// }
 
-    Week.findOne({ userOwner, weekCommencing })
-        .then(Week => {
-            const entriesFromToday = Week[`${currentDay}`];
-            res.json({ entriesFromToday }).status(200);
-        })
-        .catch(err => {
-            console.error(err);
-            res.json({ "message": "Error getting today's entries, please try again later." }).status(400);
-        });
-}
+// exports.week_updateEntries_post = async (req, res) => {
+//     const { id, dayOfWeek, foodItem, userOwner } = req.body;
+//     try {
+//         Week.findOne({ id, userOwner })
+//             .then(Week => {
+//                 Week[dayOfWeek].addToSet(foodItem);
+//                 Week.save();
+//                 res.json({ "message": "New item added successfully! Selected week updated." }).status(201);
+//             })
+//             .catch(err => {
+//                 console.error(err);
+//                 res.json({ "message": "Unable to add item and update week - please try again later." }).status(400);
+//             })
 
-exports.week_detailById_put = (req, res) => {
-    const { id, userOwner } = req.body;
-    Week.findOne({ id, userOwner })
-        .then(Week => {
-            res.json({ Week }).status(200);
-        })
-        .catch(err => {
-            console.error(err);
-            res.json({ "message": "Error getting week by id, please try again later." }).status(400);
-        });
-}
+//     } catch (err) {
+//         console.log(err);
+//         res.status(500).send('Server error');
+//     }
+// }
 
-exports.week_dailyDetailById_put = (req, res) => {
-    const { id, day, userOwner } = req.body;
-    Week.findOne({ id, userOwner })
-        .then(Week => {
-            const dayDetail = Week[day];
-            res.json({ dayDetail }).status(200);
-        })
-        .catch(err => {
-            console.error(err);
-            res.json({ "message": "Error getting day by id, please try again later." }).status(400);
-        });
-}
+// exports.week_deleteAll = async (req, res) => {
+//     const { id, userOwner } = req.body;
+//     try {
+//         await Week.findOne({ id, userOwner }).deleteOne();
+//         res.json({ "message": "Week successfully deleted." }).status(200);
+//     } catch (err) {
+//         console.error(err);
+//         res.json({ "message": "Error deleting week." }).status(400);
+//     }
+// }
 
-exports.week_updateEntries_post = async (req, res) => {
-    const { id, dayOfWeek, foodItem, userOwner } = req.body;
-    try {
-        Week.findOne({ id, userOwner })
-            .then(Week => {
-                Week[dayOfWeek].addToSet(foodItem);
-                Week.save();
-                res.json({ "message": "New item added successfully! Selected week updated." }).status(201);
-            })
-            .catch(err => {
-                console.error(err);
-                res.json({ "message": "Unable to add item and update week - please try again later." }).status(400);
-            })
+// exports.week_deleteDay = async (req, res) => {
+//     const { id, dayOfWeek, userOwner } = req.body;
+//     try {
+//         Week.findOne({ id, userOwner })
+//             .then(Week => {
+//                 Week[dayOfWeek] = [];
+//                 Week.save();
+//                 res.json({ "message": "Day successfully deleted from week." }).status(200);
+//             })
+//             .catch(err => {
+//                 console.error(err);
+//                 res.json({ "message": "Error deleting day from week." }).status(400);
+//             })
+//     } catch (err) {
+//         console.log(err);
+//         res.status(500).send('Server error');
+//     }
+// }
 
-    } catch (err) {
-        console.log(err);
-        res.status(500).send('Server error');
-    }
-}
-
-exports.week_deleteAll = async (req, res) => {
-    const { id, userOwner } = req.body;
-    try {
-        await Week.findOne({ id, userOwner }).deleteOne();
-        res.json({ "message": "Week successfully deleted." }).status(200);
-    } catch (err) {
-        console.error(err);
-        res.json({ "message": "Error deleting week." }).status(400);
-    }
-}
-
-exports.week_deleteDay = async (req, res) => {
-    const { id, dayOfWeek, userOwner } = req.body;
-    try {
-        Week.findOne({ id, userOwner })
-            .then(Week => {
-                Week[dayOfWeek] = [];
-                Week.save();
-                res.json({ "message": "Day successfully deleted from week." }).status(200);
-            })
-            .catch(err => {
-                console.error(err);
-                res.json({ "message": "Error deleting day from week." }).status(400);
-            })
-    } catch (err) {
-        console.log(err);
-        res.status(500).send('Server error');
-    }
-}
-
-exports.week_deleteEntry = (req, res) => {
-    const { id, dayOfWeek, foodItem, userOwner } = req.body;
-    try {
-        Week.findOne({ id, userOwner })
-            .then(Week => {
-                Week[dayOfWeek].pull(foodItem);
-                Week.save();
-                res.json({ "message": "Food item successfully deleted from day's entries." }).status(200);
-            })
-            .catch(err => {
-                console.error(err);
-                res.json({ "message": "Error deleting food item from day's entries." }).status(400);
-            })
-    } catch (err) {
-        console.log(err);
-        res.status(500).send('Server error');
-    }
-}
+// exports.week_deleteEntry = (req, res) => {
+//     const { id, dayOfWeek, foodItem, userOwner } = req.body;
+//     try {
+//         Week.findOne({ id, userOwner })
+//             .then(Week => {
+//                 Week[dayOfWeek].pull(foodItem);
+//                 Week.save();
+//                 res.json({ "message": "Food item successfully deleted from day's entries." }).status(200);
+//             })
+//             .catch(err => {
+//                 console.error(err);
+//                 res.json({ "message": "Error deleting food item from day's entries." }).status(400);
+//             })
+//     } catch (err) {
+//         console.log(err);
+//         res.status(500).send('Server error');
+//     }
+// }

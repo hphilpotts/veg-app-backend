259c17d743e4c8ce16c48d4ab14ae596c54a170d
Henry Philpotts <hphilpotts@gmail.com>
2023-12-02 18:42:12 +0000

    Readded auth checks to week routes.

--- app/routes/week.js
+++ app/routes/week.js
@@ -8,10 +8,10 @@ const weekController = require('../controllers/week');
 const isLoggedInCheck = require('../middleware/authentication');
 const authorisationCheck = require('../middleware/authorisation');
 
-router.post('/week/create', weekController.week_create_post);
-router.get('/week/index', weekController.week_index_get);
-router.get('/week/find', weekController.week_byDate_get);
-router.put('/week/update', weekController.week_update_post);
-router.delete('/week/delete', weekController.week_delete);
+router.post('/week/create', authorisationCheck, weekController.week_create_post); // req.body: user, date
+router.get('/week/index', isLoggedInCheck, weekController.week_index_get); // req.query: user
+router.get('/week/find', isLoggedInCheck, weekController.week_byDate_get); // req.query: user, date
+router.put('/week/update', authorisationCheck, weekController.week_update_post); // req.body: id, day, newData
+router.delete('/week/delete', authorisationCheck, weekController.week_delete); // req.body: id
 
 module.exports = router;

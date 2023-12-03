74edaf01eb2a31f5ef9b3aabcf73213e1cc50064
Henry Philpotts <hphilpotts@gmail.com>
2023-12-03 11:39:54 +0000

    Updates to auth middleware

--- app/middleware/authentication.js
+++ app/middleware/authentication.js
@@ -5,24 +5,24 @@ module.exports = function (req, res, next) {
     const token = req.header('x-auth-token');
 
     if (!token) {
-        return res.status(401).json({ "message": 'No token found, authorisation denied' });
-    }
+        return res.status(401).json({ "message": 'No token found, authorisation denied.' });
+    };
 
     try {
         jwt.verify(token, process.env.SECRET, (error, decoded) => {
             if (error) {
-                return res.status(401).json({ "message": 'Token provided is not valid, authorisation denied' });
+                return res.status(401).json({ "message": 'Token provided is not valid, authorisation denied.' });
             } else {
                 req.user = decoded.user;
                 next();
             }
         });
 
-    } catch (err) {
+    } catch (error) {
 
         console.error('something wrong with auth middleware');
-        console.error(err);
+        console.error(error);
         res.status(500).json({ "message": 'Server Error' });
 
-    }
+    };
 };
--- app/middleware/authorisation.js
+++ app/middleware/authorisation.js
@@ -2,39 +2,36 @@ const jwt = require('jsonwebtoken');
 
 module.exports = function (req, res, next) {
 
-    console.log(req.body);
-
     const token = req.header('x-auth-token');
-    const userOwner = req.body.userOwner;
+    const user = req.body.user;
 
     if (!token) {
-        return res.status(401).json({ "message": 'No token found, authorisation denied' });
-    }
+        return res.status(401).json({ "message": "No token found, authorisation denied" });
+    };
 
     try {
 
         jwt.verify(token, process.env.SECRET, (error, decoded) => {
+
             if (error) {
-                return res.status(401).json({ "message": 'Token provided is not valid, authorisation denied' });
+                return res.status(401).json({ "message": "Token provided is not valid, authorisation denied" });
             } else {
                 req.user = decoded.user;
-            }
-        })
+            };
 
-        console.log('middleware is comparing the req.user.id to the Week user owner:');
-        console.log(req.user.id, userOwner)
+        });
 
-        if (req.user.id === userOwner) {
+        if (req.user.id === user) {
             next();
         } else {
-            return res.status(403).json({ "message": 'Authorisation denied, user making request is not permitted access.' });
-        }
+            return res.status(403).json({ "message": "Authorisation denied, user making request is not permitted access." });
+        };
 
-    } catch (err) {
+    } catch (error) {
 
-        console.error('something wrong with auth middleware');
-        res.status(500).json({ msg: 'Server Error' });
+        console.error(error);
+        res.status(500).json({ "message": "Server error (authorisation middleware)." });
 
-    }
+    };
 
-}
+};
